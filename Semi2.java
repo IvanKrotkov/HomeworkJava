614cfb678714a63111dd09b83692c021cdc0a52f
Ivan <csgo.dr@mail.ru>
2023-02-18 15:36:11 +0700

    add read file

--- Semi2.java
+++ Semi2.java
@@ -1,12 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class Semi2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int res = exponentiation(4,4);
-        System.out.println(res);
+        fileReading();
 
     }
     static int exponentiation(int a, int b){
         if(b==0) return 1;
         return a*exponentiation(a,b-1);
     }
+    static String[] fileReading() throws IOException {
+        Path file = Path.of("C:/Users/krotk/OneDrive/Рабочий стол/HomeworkJava/Homework_GB/src/input.txt");
+        String[] fileArray = Files.readAllLines(file).toArray(new String[0]);
+//        System.out.println(Arrays.toString(fileArray));
+        return  fileArray;
+    }
+
 
 }
