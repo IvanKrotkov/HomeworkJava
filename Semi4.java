dc080e65fc184f7250ff767be6f2b96753343749
Ivan <csgo.dr@mail.ru>
2023-02-25 14:35:59 +0700

    update hmwrk 4

--- Semi4.java
+++ Semi4.java
@@ -1,26 +1,26 @@
 import java.util.Arrays;
 
 public class Semi4 {
+    static void swap(int whatAreChangeInd,int whatDoChangeInd,int[] arr){
+        int temp = arr[whatAreChangeInd];
+        arr[whatAreChangeInd] = arr[whatDoChangeInd];
+        arr[whatDoChangeInd] = temp;
+    }
     static void sorting(int[] arr) {
         int len = arr.length;
         for (int i = len / 2 - 1; i >= 0; i--) {
             createHeap(arr, len, i);
         }
         for (int i = len - 1; i >= 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
-
+            swap(0,i,arr);
             createHeap(arr, i, 0);
         }
     }
-
     public static void main(String[] args) {
         int[] array = {32, 524, 3, 65, 43, 3, 5, 7, 7, 5, 43, 3, 25, 5, 4};
         sorting(array);
         System.out.println(Arrays.toString(array));
     }
-
     static void createHeap(int[] arr, int len, int index) {
         int max = index;
         int left = 2 * index + 1;
@@ -32,9 +32,7 @@ public class Semi4 {
             max = right;
         }
         if (max != index) {
-            int temp = arr[index];
-            arr[index] = arr[max];
-            arr[max] = temp;
+            swap(index,max,arr);
             createHeap(arr, len, max);
         }
     }
