2b82afb33d93eec8459f0b3ba411672e9b2f1183
Ivan <csgo.dr@mail.ru>
2023-03-04 18:32:29 +0700

    seminar 6 hmwrk

--- README.md
+++ README.md
@@ -211,7 +211,158 @@ static void createOutlineMap(int[][] map){
         }
     }
 ```
+# Задача шестого семинара - [URL](Semi6.java)
+*Реализовать волновой алгоритм используя классы*
+1. Класс Coordinate - класс координат
+```Java
+static class Coordinate {
+        int x;
+        int y;
 
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        static boolean belongingToTheMap(int[][] map, Coordinate ex) {
+            if (ex.x < 0 & ex.y < 0 | ex.x > map.length & ex.y > map[0].length) return false;
+            else return true;
+        }
+    }
+```
+2. Класс MapCreater - все действия с картой 
+```Java
+static class MapCreater {
+            static int[][] createEmptyMap(int rows, int columns) {
+            return new int[rows][columns];
+        }
+        static int[][] getMap() {
+            return new int[][]{
+                    {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, -1, -1, -1, -1, 00, 00, 00, 00, -1, 00, -1, -1, -1, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, -1, -1, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, -1, -1, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -3, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+            };
+        }
+        private static void createOutlineMap(int[][] map) {
+            for (int i = 0; i < map[0].length; i++) {
+                map[0][i] = -1;
+                map[map.length - 1][i] = -1;
+            }
+            for (int i = 0; i < map.length; i++) {
+                map[i][0] = -1;
+                map[i][map[i].length - 1] = -1;
+            }
+        }
+
+        private static void createWall(int[][] map) {
+            int row = new Random().nextInt(2, map.length);
+            int column = new Random().nextInt(3, map[0].length);
+            for (int i = row; i < map.length - 4; i++) {
+                map[i][column] = -1;
+            }
+            for (int i = column; i < map[0].length - 4; i++) {
+                map[row][i] = -1;
+            }
+        }
+
+        static int[][] getRandomMap(int rows, int columns) {
+            int[] map[] = createEmptyMap(rows, columns);
+            createOutlineMap(map);
+            createWall(map);
+            createWall(map);
+            createWall(map);
+            return map;
+        }
+
+        static void printMap(int[][] map) {
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++) {
+                    System.out.printf("%5d", map[i][j]);
+                }
+                System.out.println();
+            }
+        }
+        static void printSolution(int[][] map,Coordinate st,Coordinate ex){
+            Queue<Coordinate> sol = AlgorithmLi.searchSolution(map,st,ex);
+            while (sol.size() != 0){
+                Coordinate step = sol.remove();
+                map[step.x][step.y] = 0;
+            }
+            printMap(map);
+        }
+        static void setStart(int[][] map, Coordinate st) {
+            if (Coordinate.belongingToTheMap(map, st)) map[st.x][st.y] = -3;
+            else return;
+        }
+        static void setExit(int[][] map, Coordinate ex) {
+            if (Coordinate.belongingToTheMap(map, ex)) {
+                map[ex.x][ex.y] = -2;
+            } else return;
+        }
+```
+3. Класс выполняющий сам волновой алгоритм 
+```Java
+public class AlgorithmLi {
+            static Queue<Coordinate> searchSolution(int[][] map, Coordinate st, Coordinate ex) {
+                Queue<Coordinate> sol = new LinkedList<>();
+                Coordinate step = ex;
+                sol.add(step);
+                while (map[step.x][step.y] != map[st.x][st.y]) {
+                    if (map[step.x - 1][step.y] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x - 1, step.y);
+                        sol.add(step);
+                    } else if (map[step.x][step.y - 1] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x, step.y - 1);
+                        sol.add(step);
+                    } else if (map[step.x + 1][step.y] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x + 1, step.y);
+                        sol.add(step);
+                    } else if (map[step.x][step.y + 1] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x, step.y + 1);
+                        sol.add(step);
+                    }
+                }
+                return sol;
+            }
+            static void wave(int[][] map, Coordinate st) {
+                Queue<Coordinate> res = new LinkedList<>();
+                map[st.x][st.y] = 1;
+                res.add(st);
+                while (res.size() > 0) {
+                    Coordinate step = res.remove();
+                    int x = step.x;
+                    int y = step.y;
+                    if (map[x - 1][y] == 0) {
+                        res.add(new Coordinate(x - 1, y));
+                        map[x - 1][y] = map[x][y] + 1;
+                    }
+                    if (map[x][y - 1] == 0) {
+                        res.add(new Coordinate(x, y - 1));
+                        map[x][y - 1] = map[x][y] + 1;
+                    }
+                    if (map[x + 1][y] == 0) {
+                        res.add(new Coordinate(x + 1, y));
+                        map[x + 1][y] = map[x][y] + 1;
+                    }
+                    if (map[x][y + 1] == 0) {
+                        res.add(new Coordinate(x, y + 1));
+                        map[x][y + 1] = map[x][y] + 1;
+                    }
+                }
+            }
+        }
+```
 
 
 
--- Semi6.java
+++ Semi6.java
@@ -1,152 +1,155 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.stream.IntStream;
+import java.util.*;
 
-public class Semi5 {
-    static int[][] getMap() {
-        return new int[][]{
-                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-                {-1, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, 00, -1, -1, -1, -1, 00, 00, 00, 00, -1, 00, -1, -1, -1, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
-                {-1, -1, -1, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, -1, -1, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -3, 00, 00, 00, 00, 00, 00, 00, -1},
-                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
-        };
+public class Semi6 {
+    static class Coordinate {
+        int x;
+        int y;
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        static boolean belongingToTheMap(int[][] map, Coordinate ex) {
+            if (ex.x < 0 & ex.y < 0 | ex.x > map.length & ex.y > map[0].length) return false;
+            else return true;
+        }
     }
-
-    static String rawData(int[][] map) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int row = 0; row < map.length; row++) {
-            for (int col = 0; col < map[row].length; col++) {
-                sb.append(String.format("%5d", map[row][col]));
+    static class MapCreater {
+            static int[][] createEmptyMap(int rows, int columns) {
+            return new int[rows][columns];
+        }
+        static int[][] getMap() {
+            return new int[][]{
+                    {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, -1, -1, -1, -1, 00, 00, 00, 00, -1, 00, -1, -1, -1, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, -1, -1, 00, -1, 00, -1, 00, 00, -1, 00, 00, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, -1, 00, 00, -1, -1, -1, 00, 00, -1, 00, -1, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, -1, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1, -1, -1, -1, -1, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, -1, -1, -1, -1, -1, -1, -1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, -3, 00, 00, 00, 00, 00, 00, 00, -1},
+                    {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+            };
+        }
+        private static void createOutlineMap(int[][] map) {
+            for (int i = 0; i < map[0].length; i++) {
+                map[0][i] = -1;
+                map[map.length - 1][i] = -1;
             }
-            sb.append("\n");
+            for (int i = 0; i < map.length; i++) {
+                map[i][0] = -1;
+                map[i][map[i].length - 1] = -1;
+            }
         }
 
-        return sb.toString();
-    }
+        private static void createWall(int[][] map) {
+            int row = new Random().nextInt(2, map.length);
+            int column = new Random().nextInt(3, map[0].length);
+            for (int i = row; i < map.length - 4; i++) {
+                map[i][column] = -1;
+            }
+            for (int i = column; i < map[0].length - 4; i++) {
+                map[row][i] = -1;
+            }
+        }
 
-    static String mapColor(int[][] map) {
-        StringBuilder sb = new StringBuilder();
+        static int[][] getRandomMap(int rows, int columns) {
+            int[] map[] = createEmptyMap(rows, columns);
+            createOutlineMap(map);
+            createWall(map);
+            createWall(map);
+            createWall(map);
+            return map;
+        }
 
-        for (int row = 0; row < map.length; row++) {
-            for (int col = 0; col < map[row].length; col++) {
-                switch (map[row][col]) {
-                    case 0:
-                        sb.append(" - ");
-                        break;
-                    case -1:
-                        sb.append("|:|");
-                        break;
-                    case -2:
-                        sb.append("< >");
-                        break;
-                    case -3:
-                        sb.append("0+0");
-                        break;
-                    default:
-                        break;
+        static void printMap(int[][] map) {
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++) {
+                    System.out.printf("%5d", map[i][j]);
+                }
+                System.out.println();
+            }
+        }
+        static void printSolution(int[][] map,Coordinate st,Coordinate ex){
+            Queue<Coordinate> sol = AlgorithmLi.searchSolution(map,st,ex);
+            while (sol.size() != 0){
+                Coordinate step = sol.remove();
+                map[step.x][step.y] = 0;
+            }
+            printMap(map);
+        }
+        static void setStart(int[][] map, Coordinate st) {
+            if (Coordinate.belongingToTheMap(map, st)) map[st.x][st.y] = -3;
+            else return;
+        }
+        static void setExit(int[][] map, Coordinate ex) {
+            if (Coordinate.belongingToTheMap(map, ex)) {
+                map[ex.x][ex.y] = -2;
+            } else return;
+        }
+        public class AlgorithmLi {
+            static Queue<Coordinate> searchSolution(int[][] map, Coordinate st, Coordinate ex) {
+                Queue<Coordinate> sol = new LinkedList<>();
+                Coordinate step = ex;
+                sol.add(step);
+                while (map[step.x][step.y] != map[st.x][st.y]) {
+                    if (map[step.x - 1][step.y] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x - 1, step.y);
+                        sol.add(step);
+                    } else if (map[step.x][step.y - 1] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x, step.y - 1);
+                        sol.add(step);
+                    } else if (map[step.x + 1][step.y] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x + 1, step.y);
+                        sol.add(step);
+                    } else if (map[step.x][step.y + 1] == map[step.x][step.y] - 1) {
+                        step = new Coordinate(step.x, step.y + 1);
+                        sol.add(step);
+                    }
+                }
+                return sol;
+            }
+            static void wave(int[][] map, Coordinate st) {
+                Queue<Coordinate> res = new LinkedList<>();
+                map[st.x][st.y] = 1;
+                res.add(st);
+                while (res.size() > 0) {
+                    Coordinate step = res.remove();
+                    int x = step.x;
+                    int y = step.y;
+                    if (map[x - 1][y] == 0) {
+                        res.add(new Coordinate(x - 1, y));
+                        map[x - 1][y] = map[x][y] + 1;
+                    }
+                    if (map[x][y - 1] == 0) {
+                        res.add(new Coordinate(x, y - 1));
+                        map[x][y - 1] = map[x][y] + 1;
+                    }
+                    if (map[x + 1][y] == 0) {
+                        res.add(new Coordinate(x + 1, y));
+                        map[x + 1][y] = map[x][y] + 1;
+                    }
+                    if (map[x][y + 1] == 0) {
+                        res.add(new Coordinate(x, y + 1));
+                        map[x][y + 1] = map[x][y] + 1;
+                    }
                 }
             }
-            sb.append("\n");
         }
-        for (int i = 0; i < 3; i++) {
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        int[] map[] = getMap();
-//        System.out.println(mapColor(map));
-        int[] st = searchPosition(map, -3);
-        wave(map, st);
-        System.out.println(rawData(map));
-//        System.out.println(mapColor(map));
-    }
-
-    static int[] searchPosition(int[][] map, int index) { // index - "-3" - точка начала;"-2" - точка выхода
-        int[] coordinate = new int[2];
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (map[i][j] == index) {
-                    coordinate[0] = i;
-                    coordinate[1] = j;
-                }
-            }
-        }
-        return coordinate;
-    }
-
-    static void print(Queue<int[]> arr) {
-        for (int[] item : arr) {
-            System.out.println("_____");
-            System.out.println(Arrays.toString(item));
-            System.out.println("-----");
-        }
-    }
-
-    static void fillAr(int[] arr, int x, int y) {
-        arr[0] = x;
-        arr[1] = y;
-    }
-
-    static void wave(int[][] map, int[] start) {
-        int[] temp1 = new int[2];
-        int[] temp2 = new int[2];
-        int[] temp3 = new int[2];
-        int[] temp4 = new int[2];
-        Queue<int[]> res = new LinkedList<>();
-        int count = 0;
-        res.add(start);
-        while (res.size() > 0) {
-            count++;
-            print(res);
-            int[] coordinate = res.remove();
-            int x = coordinate[0];
-            int y = coordinate[1];
-            if (Arrays.equals(coordinate, start)) {
-                map[x][y] = 1;
-            }
-            if (map[x - 1][y] == 0) {
-                temp1[0] = x - 1;
-                temp1[1] = y;
-                res.add(temp1);
-                System.out.println(Arrays.toString(temp1));
-                map[x - 1][y] = map[x][y] + 1;
-            }
-            if (map[x][y + 1] == 0) {
-                fillAr(temp2, x, (y + 1));
-                res.add(temp2);
-                System.out.println(Arrays.toString(temp2));
-                map[x][y + 1] = map[x][y] + 1;
-            }
-            if (map[x + 1][y] == 0) {
-                fillAr(temp3, (x + 1), y);
-                res.add(temp3);
-                System.out.println(Arrays.toString(temp3));
-                map[x + 1][y] = map[x][y] + 1;
-
-            }
-            if (map[x][y - 1] == 0) {
-                fillAr(temp4, x, (y - 1));
-                res.add(temp4);
-                System.out.println(Arrays.toString(temp4));
-                map[x][y - 1] = map[x][y] + 1;
-            }
-            print(res);
+        public static void main(String[] args) {
+            int[] map[] = MapCreater.getRandomMap(10, 20);
+            Coordinate exit = new Coordinate(8, 10);
+            Coordinate start = new Coordinate(6, 2);
+            MapCreater.setStart(map, start);
+            AlgorithmLi.wave(map, start);
+            MapCreater.printMap(map);
+            System.out.println("--");
+            MapCreater.printSolution(map,start,exit);
         }
     }
 }
